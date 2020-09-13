1f528c14d130079eb89ead98db095ce9f69f6303
vivek <vivek@vivek-VirtualBox>
2020-09-13 21:32:42 +0530

    updated comments

--- 13septpoc/src/com/example/MyHelloWorld.java
+++ 13septpoc/src/com/example/MyHelloWorld.java
@@ -3,7 +3,7 @@ package com.example;
 public class MyHelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("This is vivek Raina !! version 1");
+		System.out.println("This is vivek Raina !! version working locally");
 	}
 
 }
