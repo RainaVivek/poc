21610606ddf9af1f5a08294c4c21fba73324f287
vivek <vivek@vivek-VirtualBox>
2020-09-13 21:51:20 +0530

    Added new feature

--- 13septpoc/src/com/example/MyHelloWorld.java
+++ 13septpoc/src/com/example/MyHelloWorld.java
@@ -3,7 +3,14 @@ package com.example;
 public class MyHelloWorld {
 	
 	public static void main(String[] args) {
+		
 		System.out.println("This is vivek Raina !! version 1 feature branch commit added!!");
+		//added new things
 	}
+	
+	private void display () {
+		System.out.println("This is a new feature added");
+	}
+	
 
 }
